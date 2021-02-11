3518fcd751beb553674f5ceae8ac212fdb3bcafb
tianaFomicheva <tianirika@gmail.com>
2021-02-11 21:30:18 +0500

    рефакторинг

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-const bodyParser = require('body-parser')
+
 
 
 const express = require('express')
 const mongoose = require('mongoose')
+const config = require('./config');
+
+require('./app/models/products')
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect('mongodb://localhost:27017/online-store')
-const Product = mongoose.model('Product', {
-    id: Number,
-    name: String,
-    price: mongoose.Schema.Types.Decimal128
-})
+mongoose.connect(config.mongoUri)
+require('./app/models/products')
+
+const {appPort, mongoUri} = config.app
+mongoose.connect(mongoUri)
+.then(()=>app.listen(appPort, ()=>console.log('Listening 3000...')))
+.catch((err)=>console.error(`Error connecting to mongo: ${mongoUri}`, err))
+
 
 const app = express()
-app.use(bodyParser.json())
+config.express(app)
+config.routes(app)
+
+
 // const products = 
 // [
 //     {
@@ -33,38 +41,7 @@ app.use(bodyParser.json())
 
 
 
-// app.get('/', (req,res)=> res.send('Привет, мир!'))
-// app.get('/products', (req,res)=> res.json(products))
-app.get('/products', (req,res)=> {Product.find()
-.exec()
-.then(products=>res.json(products))
-})
-
-app.post('/products',  (req,res)=> {
-    Product.create(req.body)
-    .then(createdProduct=>res.json(createdProduct))
-})
-// app.post('/products',  (req,res)=> {
-//     products.push(req.body)
-//     res.json(req.body)})
-
-    app.put('/products/:id',  (req,res)=> {Product.findOneAndUpdate({id: req.params.id}, req.body)
-        .exec()
-        .then(product=>res.json(product))
 
 
 
-    })
-    app.delete('/products/:id',  (req,res)=> {Product.deleteOne({id: req.params.id})
-    .exec()
-    .then(()=>res.json({success: true}))
-})
-    app.put('/products/:id',  (req,res)=> {
-        const product = products.find(p => p.id === + req.params.id)
-        const productIndex = products.indexOf(product)
-        const newProduct = {...product, ...req.body}
-        products[productIndex] = newProduct
-        res.json({success: true})
-    })    
 //eslint-disable-next-line
-app.listen(3000, ()=>console.log('Listening 3000...'))
